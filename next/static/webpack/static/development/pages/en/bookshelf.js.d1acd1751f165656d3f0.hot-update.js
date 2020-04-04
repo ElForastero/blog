6cedb3bbf330908aaedc234662cf3680b105d91c
Eugene Dzhumak <elforastero@ya.ru>
2020-04-04 12:11:33 +0400

    Squashed 'out/' content from commit 01e8e61
    git-subtree-dir: out
    git-subtree-split: 01e8e61a9bbe1acf8c4214ec516a73b99a58851b

---
+++ next/static/webpack/static/development/pages/en/bookshelf.js.d1acd1751f165656d3f0.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static/development/pages/en/bookshelf.js",{
+
+/***/ "./src/locales/en/common.json":
+/*!************************************!*\
+  !*** ./src/locales/en/common.json ***!
+  \************************************/
+/*! exports provided: bookshelf, contact, cv, show_all, latest_articles, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"bookshelf\":\"Bookshelf\",\"contact\":\"Contact\",\"cv\":\"CV\",\"show_all\":\"Show All\",\"latest_articles\":\"Latest Articles\"}");
+
+/***/ })
+
+})
+//# sourceMappingURL=bookshelf.js.d1acd1751f165656d3f0.hot-update.js.map
