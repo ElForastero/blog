8bb5285c8422c56cebef9eb4f06e696c767d2270
Eugene Dzhumak <elforastero@ya.ru>
2020-04-04 13:00:23 +0400

    :rocket: Deploy

--- out/_next/static/DfpG5rCyMtnwNDyAeGowB/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fen\u002Fblog","\u002Fen","\u002F","\u002Fru\u002Fblog","\u002Fru","\u002Fblog\u002F[slug]","\u002Fen\u002Fblog\u002F[slug]","\u002Fru\u002Fblog\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
